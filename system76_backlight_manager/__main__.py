b2d691aacff0db3058bfdc728457e09c522c5a51
Jeff Labonte <grimsleepless@protonmail.com>
2021-02-12 21:34:43 -0500

    Fix code to work with changes

--- system76_backlight_manager/__main__.py
+++ system76_backlight_manager/__main__.py
@@ -4,12 +4,14 @@ import os
 import yaml
 from cerberus import Validator
 
-from battery_backlight.battery import Battery
-from battery_backlight.common import read_file
-from battery_backlight.keyboard_backlight import KeyboardBacklight
-from battery_backlight.schema.configurations import schema as configurations_schema
+from system76_backlight_manager.battery import Battery
+from system76_backlight_manager.common import read_file
+from system76_backlight_manager.keyboard_backlight import KeyboardBacklight
+from system76_backlight_manager.schema.configurations import (
+    schema as configurations_schema,
+)
 
-CONFIGURATION_PATH = '/etc/battery-backlight.conf'
+CONFIGURATION_PATH = "/etc/system76-backlight-manager.conf"
 
 
 def validate_configs(configurations):
@@ -17,14 +19,14 @@ def validate_configs(configurations):
     if not validator.validate(configurations):
         raise RuntimeError(validator.errors)
 
-    if max_value := configurations.get('brightness_max_value'):
-        if max_value <= configurations.get('brightness_min_value'):
+    if max_value := configurations.get("brightness_max_value"):
+        if max_value <= configurations.get("brightness_min_value"):
             raise RuntimeError(
                 "Expect brightness_min_value to be greater than brightness_max_value"
             )
 
-    if yellow_threshold := configurations.get('yellow_threshold'):
-        if yellow_threshold <= configurations.get('red_threshold'):
+    if yellow_threshold := configurations.get("yellow_threshold"):
+        if yellow_threshold <= configurations.get("red_threshold"):
             raise RuntimeError(
                 "Expect yellow_threshold to be greater than red_threshold"
             )
@@ -32,10 +34,7 @@ def validate_configs(configurations):
 
 def read_configurations():
     if os.path.exists(CONFIGURATION_PATH):
-        confs = yaml.load(
-            read_file(CONFIGURATION_PATH),
-            Loader=yaml.FullLoader
-        )
+        confs = yaml.load(read_file(CONFIGURATION_PATH), Loader=yaml.FullLoader)
         validate_configs(confs)
         return confs
 
@@ -46,13 +45,10 @@ def main():
     config = read_configurations()
 
     battery = Battery()
-    kb_backlight = KeyboardBacklight(
-        context=config,
-        battery_handler=battery
-    )
+    kb_backlight = KeyboardBacklight(context=config, battery_handler=battery)
     print("Starting Service")
     kb_backlight.run()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
