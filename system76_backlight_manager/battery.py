ece7f9498580ee43a2b505a777de77853ac33848
Jeff Labonte <grimsleepless@protonmail.com>
2021-02-20 00:07:08 -0500

    Update Battery Class

--- system76_backlight_manager/battery.py
+++ system76_backlight_manager/battery.py
@@ -5,14 +5,14 @@ class Battery:
     FULL_BATTERY_PATH = "/sys/class/power_supply/BAT0/charge_full"
     CURRENT_BATTERY_PATH = "/sys/class/power_supply/BAT0/charge_now"
 
-    def _read_current_battery(self):
-        return read_file(self.CURRENT_BATTERY_PATH)
+    def _read_current_battery(self) -> int:
+        return int(read_file(self.CURRENT_BATTERY_PATH))
 
-    def _read_full_battery(self):
-        return read_file(self.FULL_BATTERY_PATH)
+    def _read_full_battery(self) -> int:
+        return int(read_file(self.FULL_BATTERY_PATH))
 
-    def get_battery_level(self):
-        current = int(self._read_current_battery())
-        full = int(self._read_full_battery())
+    def get_battery_level(self) -> int:
+        current = self._read_current_battery()
+        full = self._read_full_battery()
 
         return (current * 100) // full
