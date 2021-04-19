b0df7d93fa1fee622cb6ec70332f46b2a950fb3d
Jeff Labonte <grimsleepless@protonmail.com>
2021-04-18 22:45:41 -0400

    Created SystemdHandler with init - WIP

--- system76_backlight_manager/handlers/systemd.py
+++ system76_backlight_manager/handlers/systemd.py
@@ -1,2 +1,14 @@
+import dbus
+
+
 class SystemdHandler:
-    pass
+    def __init__(self, unit_name):
+        self.system_dbus = dbus.SystemBus()
+        self.systemd = self.system_dbus.get_object(
+            "org.freedesktop.systemd1",
+            "/org/freedesktop/systemd1",
+        )
+        self.manager = dbus.Interface(
+            object=self.systemd,
+            dbus_interface="org.freedesktop.systemd1.Manager",
+        )
