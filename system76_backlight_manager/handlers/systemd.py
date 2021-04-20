3e4471e37ae11a60f674b3325c3326cc63c0d04d
Jeff Labonte <grimsleepless@protonmail.com>
2021-04-19 21:59:21 -0400

    Create handler for systemd

--- system76_backlight_manager/handlers/systemd.py
+++ system76_backlight_manager/handlers/systemd.py
@@ -2,7 +2,9 @@ import dbus
 
 
 class SystemdHandler:
-    def __init__(self, unit_name):
+    MODE = "fail"
+
+    def __init__(self, unit_name: str):
         self.system_dbus = dbus.SystemBus()
         self.systemd = self.system_dbus.get_object(
             "org.freedesktop.systemd1",
@@ -12,3 +14,17 @@ class SystemdHandler:
             object=self.systemd,
             dbus_interface="org.freedesktop.systemd1.Manager",
         )
+        self.unit_name = unit_name
+
+    def get_status(self) -> bool:
+        status_str = str(self.manager.GetUnitFileState(self.unit_name))
+        return status_str == "enabled"
+
+    def stop_service(self) -> None:
+        self.manager.StopUnit(self.unit_name, self.MODE)
+
+    def start_service(self) -> None:
+        self.manager.StartUnit(self.unit_name, self.MODE)
+
+    def enable_service(self) -> None:
+        pass
